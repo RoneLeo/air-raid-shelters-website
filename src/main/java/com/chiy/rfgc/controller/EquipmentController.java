519ac0ba9a10c1dd200dd3854639b1f126694107
lihuan <964739148@qq.com>
2018-12-28 16:50:02 +0800

    添加

--- src/main/java/com/chiy/rfgc/controller/EquipmentController.java
+++ src/main/java/com/chiy/rfgc/controller/EquipmentController.java
@@ -9,12 +9,14 @@ import com.chiy.rfgc.repository.UserRepository;
 import com.chiy.rfgc.utils.StringUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import java.util.Date;
@@ -33,9 +35,20 @@ public class EquipmentController {
     @Resource
     private EquipmentTypeRepository equipmentTypeRepository;
 
+    private static final String EQUIPMENT_PHOTO_PATH = "/equipment/";
+
     @ApiOperation(value = "添加")
     @RequestMapping("/add")
-    public ApiResult<Object> add(String uuid, EquipmentEntity entity) {
+    public ApiResult<Object> add(String uuid, EquipmentEntity entity,
+                                 @ApiParam(value = "产品特点图片") MultipartFile cptdtp1,
+                                 @ApiParam(value = "适用范围图片") MultipartFile syfwtp1,
+                                 @ApiParam(value = "主要技术参数图片") MultipartFile zyjscstp1,
+                                 @ApiParam(value = "单位图片") MultipartFile dwtp1,
+                                 @ApiParam(value = "预留备用穿墙管间距尺寸表图片") MultipartFile ylbtp1,
+                                 @ApiParam(value = "说明图片") MultipartFile smtp1,
+                                 @ApiParam(value = "安装与使用图片") MultipartFile azysytp1,
+                                 @ApiParam(value = "性能图片") MultipartFile xntp1,
+                                 @ApiParam(value = "分类图片") MultipartFile fltp1) {
         // 判断是否登录
         if (StringUtils.isEmpty(uuid) || userRepository.findByUuid(uuid) == null) {
             return ApiResult.FAILURE("未登录");
@@ -48,6 +61,33 @@ public class EquipmentController {
         if (entity.getSblx() == null || equipmentTypeRepository.findById(entity.getSblx()) == null) {
             return ApiResult.FAILURE("添加失败，设备类型不能为空或该设备类型不存在");
         }
+        if (cptdtp1 != null) {
+            entity.setCptdtp(EQUIPMENT_PHOTO_PATH + cptdtp1.getOriginalFilename());
+        }
+        if (syfwtp1 != null) {
+            entity.setSyfwtp(EQUIPMENT_PHOTO_PATH + syfwtp1.getOriginalFilename());
+        }
+        if (zyjscstp1 != null) {
+            entity.setZyjscstp(EQUIPMENT_PHOTO_PATH + zyjscstp1.getOriginalFilename());
+        }
+        if (dwtp1 != null) {
+            entity.setDwtp(EQUIPMENT_PHOTO_PATH + dwtp1.getOriginalFilename());
+        }
+        if (ylbtp1 != null) {
+            entity.setYlbtp(EQUIPMENT_PHOTO_PATH + ylbtp1.getOriginalFilename());
+        }
+        if (smtp1 != null) {
+            entity.setSmtp(EQUIPMENT_PHOTO_PATH + smtp1.getOriginalFilename());
+        }
+        if (azysytp1 != null) {
+            entity.setAzysytp(EQUIPMENT_PHOTO_PATH + azysytp1.getOriginalFilename());
+        }
+        if (xntp1 != null) {
+            entity.setXntp(EQUIPMENT_PHOTO_PATH + xntp1.getOriginalFilename());
+        }
+        if (fltp1 != null) {
+            entity.setFltp(EQUIPMENT_PHOTO_PATH + fltp1.getOriginalFilename());
+        }
         entity.setCjsj(new Date());
         EquipmentEntity entity1 = equipmentRepository.save(entity);
         if (entity1 == null) {
@@ -58,7 +98,16 @@ public class EquipmentController {
 
     @ApiOperation("修改")
     @RequestMapping("/update")
-    public ApiResult<Object> update(String uuid, EquipmentEntity entity) {
+    public ApiResult<Object> update(String uuid, EquipmentEntity entity,
+                                    @ApiParam(value = "产品特点图片") MultipartFile cptdtp1,
+                                    @ApiParam(value = "适用范围图片") MultipartFile syfwtp1,
+                                    @ApiParam(value = "主要技术参数图片") MultipartFile zyjscstp1,
+                                    @ApiParam(value = "单位图片") MultipartFile dwtp1,
+                                    @ApiParam(value = "预留备用穿墙管间距尺寸表图片") MultipartFile ylbtp1,
+                                    @ApiParam(value = "说明图片") MultipartFile smtp1,
+                                    @ApiParam(value = "安装与使用图片") MultipartFile azysytp1,
+                                    @ApiParam(value = "性能图片") MultipartFile xntp1,
+                                    @ApiParam(value = "分类图片") MultipartFile fltp1) {
         // 判断是否登录
         if (StringUtils.isEmpty(uuid) || userRepository.findByUuid(uuid) == null) {
             return ApiResult.FAILURE("未登录");
@@ -70,6 +119,33 @@ public class EquipmentController {
         // 判断设备类型
         if (entity.getSblx() == null || equipmentTypeRepository.findById(entity.getSblx()) == null) {
             return ApiResult.FAILURE("修改失败，设备类型不能为空或该设备类型不存在");
+        }
+        if (cptdtp1 != null) {
+            entity.setCptdtp(EQUIPMENT_PHOTO_PATH + cptdtp1.getOriginalFilename());
+        }
+        if (syfwtp1 != null) {
+            entity.setSyfwtp(EQUIPMENT_PHOTO_PATH + syfwtp1.getOriginalFilename());
+        }
+        if (zyjscstp1 != null) {
+            entity.setZyjscstp(EQUIPMENT_PHOTO_PATH + zyjscstp1.getOriginalFilename());
+        }
+        if (dwtp1 != null) {
+            entity.setDwtp(EQUIPMENT_PHOTO_PATH + dwtp1.getOriginalFilename());
+        }
+        if (ylbtp1 != null) {
+            entity.setYlbtp(EQUIPMENT_PHOTO_PATH + ylbtp1.getOriginalFilename());
+        }
+        if (smtp1 != null) {
+            entity.setSmtp(EQUIPMENT_PHOTO_PATH + smtp1.getOriginalFilename());
+        }
+        if (azysytp1 != null) {
+            entity.setAzysytp(EQUIPMENT_PHOTO_PATH + azysytp1.getOriginalFilename());
+        }
+        if (xntp1 != null) {
+            entity.setXntp(EQUIPMENT_PHOTO_PATH + xntp1.getOriginalFilename());
+        }
+        if (fltp1 != null) {
+            entity.setFltp(EQUIPMENT_PHOTO_PATH + fltp1.getOriginalFilename());
         }
         EquipmentEntity entity1 = equipmentRepository.save(entity);
         if (entity1 == null) {
