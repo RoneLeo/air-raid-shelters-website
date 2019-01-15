9ea9428cf63e04e1ee2b857ac8742f0d0682c06a
lihuan <964739148@qq.com>
2019-01-15 16:52:56 +0800

    设置文件上传大小

--- src/main/java/com/chiy/rfgc/RfgcApplication.java
+++ src/main/java/com/chiy/rfgc/RfgcApplication.java
@@ -43,17 +43,17 @@ public class RfgcApplication {
         };
     }
 
-    @Bean
-    public MultipartConfigElement multipartConfigElement() {
-        MultipartConfigFactory factory = new MultipartConfigFactory();
-        // 设置文件大小限制 ,超出设置页面会抛出异常信息，
-        // 这样在文件上传的地方就需要进行异常信息的处理了;
-        factory.setMaxFileSize("2024MB"); // KB,MB
-        /// 设置总上传数据总大小
-        factory.setMaxRequestSize("2024MB");
-        // Sets the directory location where files will be stored.
-        // factory.setLocation("路径地址");
-        return factory.createMultipartConfig();
-    }
+//    @Bean
+//    public MultipartConfigElement multipartConfigElement() {
+//        MultipartConfigFactory factory = new MultipartConfigFactory();
+//        // 设置文件大小限制 ,超出设置页面会抛出异常信息，
+//        // 这样在文件上传的地方就需要进行异常信息的处理了;
+//        factory.setMaxFileSize("2024MB"); // KB,MB
+//        /// 设置总上传数据总大小
+//        factory.setMaxRequestSize("2024MB");
+//        // Sets the directory location where files will be stored.
+//        // factory.setLocation("路径地址");
+//        return factory.createMultipartConfig();
+//    }
 }
 
